93f89ba83fb65f979f23663f40cc3ccdb4f1d93f
jayasenthil <jayasenthilceg@gmail.com>
2017-05-06 10:02:43 +0530

    Basic template

---
+++ js/script.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+	
+})
+
+
+chrome.browserAction.onClicked.addListener(function(tab) {
+chrome.tabs.executeScript({
+    code: 'document.body.style.backgroundColor="green"'
+});
+});
