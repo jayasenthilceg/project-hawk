43e6b5601d79a26fdd4d6deafd4498aa867c5aa9
jayasenthil <jayasenthilceg@gmail.com>
2017-05-06 13:38:47 +0530

    setting local storage datas

--- src/bg/background.js
+++ src/bg/background.js
@@ -1,11 +1,65 @@
-// var settings = new Store("settings", {
-//     "sample_setting": "This is how you use Store.js to remember values"
-// });
+// // var settings = new Store("settings", {
+// //     "sample_setting": "This is how you use Store.js to remember values"
+// // });
 
 
-//example of using a message handler from the inject scripts
-chrome.extension.onMessage.addListener(
+// //example of using a message handler from the inject scripts
+// chrome.extension.onMessage.addListener(
+//   function(request, sender, sendResponse) {
+//   	chrome.pageAction.show(sender.tab.id);
+//     sendResponse();
+//   });
+
+
+// chrome-ext background.js starts
+
+
+function clearStats() {
+  if (config.clearStatsInterval < 3600) {
+    config.nextTimeToClear = 0;
+    return;
+  }
+
+  if (!config.nextTimeToClear) {
+    var d = new Date();
+    d.setTime(d.getTime() + config.clearStatsInterval * 1000);
+    d.setMinutes(0);
+    d.setSeconds(0);
+    if (config.clearStatsInterval > 3600) {
+      d.setHours(0);
+    }
+    config.nextTimeToClear = d.getTime();
+  }
+  var now = new Date();
+  if (now.getTime() > config.nextTimeToClear) {
+    sites.clear();
+    var nextTimeToClear = new Date(nextTimeToClear + config.clearStatsInterval * 1000);
+    config.nextTimeToClear = nextTimeToClear.getTime();
+    return;
+  }
+}
+
+var config = new Config();
+var sites = new Sites(config);
+var tracker = new Tracker(config, sites);
+
+/* Listen for requests which come from the user through the popup. */
+chrome.extension.onRequest.addListener(
   function(request, sender, sendResponse) {
-  	chrome.pageAction.show(sender.tab.id);
-    sendResponse();
+    if (request.action == "clearStats") {
+      sites.clear();
+      sendResponse({});
+    } else if (request.action == "addIgnoredSite") {
+      config.addIgnoredSite(request.site);
+      sendResponse({});
+    } else {
+      console.log("Invalid action given: " + request.action);
+    }
   });
+
+chrome.alarms.create("clearStats", {periodInMinutes: 2});
+chrome.alarms.onAlarm.addListener(function(alarm) {
+  if (alarm.name == "clearStats") {
+    clearStats(config);
+  }
+});
